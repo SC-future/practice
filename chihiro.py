0a439eba7180c02cc58e2cfc60f212187b7ac5d5
SC-future <mario.continue24-ars@yahoo.ne.jp>
2020-04-29 15:55:13 +0900

    Janomeによる形態素解析を実装

--- chihiro.py
+++ chihiro.py
@@ -1,4 +1,11 @@
 import pandas as pd
+from janome.tokenizer import Tokenizer
+
+t = Tokenizer()
+
+s = 'すもももももももものうち'
+for token in t.tokenize(s):
+    print(token)
 
 print('Hello World!')
 print('鬼滅の刃は面白い')
