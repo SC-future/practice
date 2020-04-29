e7b7f2e4a56a548789850ed0ac45b506dedbdfb6
SC-future <mario.continue24-ars@yahoo.ne.jp>
2020-04-29 15:57:47 +0900

    PRGにコメントを追加

--- chihiro.py
+++ chihiro.py
@@ -1,11 +1,10 @@
-import pandas as pd
-from janome.tokenizer import Tokenizer
+import pandas as pd # pandas
+from janome.tokenizer import Tokenizer # Janome
 
 t = Tokenizer()
-
-s = 'すもももももももものうち'
+s = 'すもももももももものうち' # 形態素解析を行うテキスト
 for token in t.tokenize(s):
-    print(token)
+    print(token) # 形態素を表示
 
-print('Hello World!')
-print('鬼滅の刃は面白い')
+print('Hello World!') # 「Hello World!」という文を出力。
+print('鬼滅の刃は面白い') # 「鬼滅の刃は面白い」という文を出力。
